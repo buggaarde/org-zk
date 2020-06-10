f564a2b39fb371137acab788478a9f4999ce98b5
Simon Bugge Siggaard <buggaarde@gmail.com>
2020-06-10 10:10:55 +0200

    add notes

--- org-zk-notes.el
+++ org-zk-notes.el
@@ -1,4 +1,4 @@
-;;; org-zk-notes --- Description of your package -*- lexical-binding: t; -*-
+;;; org-zk-notes --- All functionality related to note creation and manipulation -*- lexical-binding: t; -*-
 ;; Copyright (c) 2020 Simon Bugge Siggaard
 
 ;; Author: Simon Bugge Siggaard <simsig@gmail.com>
@@ -12,22 +12,20 @@
 
 ;;; Code:
 
-(require 'org-zk)
+(defvar org-zk-directory)
 
 (defun org-zk-new-note ()
   "Create a new zettel and open file in buffer."
-  (let ((filename
+  (interactive)
+  (let ((fname
 		 (concat org-zk-directory (format-time-string "%Y%m%d%H%M%S") ".org")))
-	(with-temp-file filename
-	  (insert "#+title:\n#+startup: showall\n** Note\n\n\n** References\n\n** Sources\n\n"))
-	(find-file filename)
-	(goto-line 5)))
+	(find-file fname)
+	(insert "#+title:\n#+startup: showall\n** Note\n\n\n** References\n\n** Sources\n\n")
+	(goto-char (point-min))
+	(let ((inhibit-message t))
+	  (forward-line (1- 4)))))
 
 
 (provide 'org-zk-notes)
-
-;; Local Variables:
-;; lexical-binding: t
-;; End:
 
 ;;; org-zk-notes.el ends here
